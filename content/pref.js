6e139a6ea723ebc860e91750a35b8cc455b352c6
erosman <erosman@users.noreply.github.com>
2019-01-04 22:35:07 -0800

    1.0
     * Initial release

---
+++ content/pref.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+// ----------------- User Preference -----------------------
+let pref = {                                                // global default
+  autoUpdateInterval: 0,
+  autoUpdateLast: 0,
+  content: {},
+  template: {
+   css: '',
+   js: '' 
+  }
+};
