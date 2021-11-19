849d92a90a4ea68f87a15c59fdb14c51ddb3d8ef
erosman <erosman@users.noreply.github.com>
2021-11-19 09:06:22 -0800

    2.38
     * Fixed GM getResourceText due to fetch returning an object (from 2.33)
     * Fixed match pattern validation error (#&#8203;391)
     * Added Brazilian Portuguese (pt_BR) locale
     * Removed *include/exclude Converter* feature due to added auto-conversion in v2.35

--- content/api.js
+++ content/api.js
@@ -285,7 +285,7 @@
         api: 'fetch',
         data: {url: resource[resourceName], init: {}}
       });
-      return response ? script.export(response) : null;
+      return response ? script.export(response.text) : null;
     },
 
     getResourceUrl(resourceName) {                          // GreaseMonkey | TamperMonkey
@@ -524,7 +524,8 @@
     GM_fetch:                     GM.fetch,
     GM_download:                  GM.download,
     GM_getResourceText:           GM.getResourceText,
-    GM_getResourceURL:            GM.getResourceUrl,
+    GM_getResourceUrl:            GM.getResourceUrl,        // GreaseMonkey | TamperMonkey
+    GM_getResourceURL:            GM.getResourceURL,        // ViolentMonkey
     GM_registerMenuCommand:       GM.registerMenuCommand,
     GM_unregisterMenuCommand:     GM.unregisterMenuCommand,
 
